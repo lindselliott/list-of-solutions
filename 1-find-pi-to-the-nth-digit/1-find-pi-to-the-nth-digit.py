03d0ab783b5ccfcd04c45d8701a7ee3b8955b12e
Lindsay Elliott <lellio04@mail.uoguelph.ca>
2018-10-29 23:25:55 -0400

    Working on this problem with exceptions

---
+++ 1-find-pi-to-the-nth-digit/1-find-pi-to-the-nth-digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import sys
+import math
+
+try:
+    dec_num = '.' + sys.argv[1] + 'f'
+    output = format(math.pi, dec_num)
+except IndexError:
+    print("Need an argument for number of decimal places.")
+    raise
+except ValueError:
+    print("Argument was not an integer.")
+    raise
+
+print(output)
